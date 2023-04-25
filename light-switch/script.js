cd4eb2cc85ba1ca9d2c82c7f2e7607795b3a3085
Kai Weluda <kaiweluda@gmail.com>
2023-04-25 10:05:57 +0200

    feat: adds an event listener for the button and toggles for button styles

---
+++ light-switch/script.js
@@ -0,0 +1,18 @@
+const title = document.querySelector("title");
+const lightSwitch = document.querySelector("button");
+// this adds a eventlistener to the button
+lightSwitch.addEventListener("click", function (event) {
+  console.log("registered click on light-switch");
+
+  // this toggles the light switch on and off
+  lightSwitch.classList.toggle("light-switch-off");
+
+  //this changes the title content
+  if (title.textContent === "Good Morning") {
+    title.textContent = "Good Night";
+  } else {
+    title.textContent = "Good Morning";
+  }
+
+  //this changes the background color
+});
