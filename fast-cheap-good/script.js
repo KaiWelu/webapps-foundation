7528c567f91cb5d97a76b16e21d978c8fb549b87
Kai Weluda <kaiweluda@gmail.com>
2023-04-27 14:35:47 +0200

    chore: refactors the function for checking the boxes

--- fast-cheap-good/script.js
+++ fast-cheap-good/script.js
@@ -1,47 +1,60 @@
-// select the checkboxes
-const buttonFast = document.querySelector("#button-fast");
-const buttonCheap = document.querySelector("#button-cheap");
-const buttonGood = document.querySelector("#button-good");
-
 function checkTheBoxes() {
-  let count = 0;
-  // look up wich boxes are checked
-  if (buttonFast.checked === true) {
-    count++;
-  }
-  if (buttonCheap.checked === true) {
-    count++;
-  }
-  if (buttonGood.checked === true) {
-    count++;
-  }
-  if (count < 3) {
-    return false;
-  } else {
-    return true;
-  }
+  const checked = document.querySelectorAll("input[type='checkbox']:checked");
+  return checked.length === 3;
 }
 
-let lastBox = null;
+let lastBox;
 
-// add eventlisteners
-buttonFast.addEventListener("click", () => {
+document.body.addEventListener("change", (event) => {
   if (checkTheBoxes() === true) {
     lastBox.checked = false;
   }
-  lastBox = buttonFast;
+  lastBox = event.target;
 });
 
-buttonCheap.addEventListener("click", () => {
-  if (checkTheBoxes() === true) {
-    lastBox.checked = false;
-  }
-  lastBox = buttonCheap;
-});
+// legacy code after this -->
 
-buttonGood.addEventListener("click", () => {
-  if (checkTheBoxes() === true) {
-    lastBox.checked = false;
-  }
-  lastBox = buttonGood;
-});
+// select the checkboxes
+// const buttonFast = document.querySelector("#button-fast");
+// const buttonCheap = document.querySelector("#button-cheap");
+// const buttonGood = document.querySelector("#button-good");
+
+// let count = 0;
+// // look up wich boxes are checked
+// if (buttonFast.checked === true) {
+//   count++;
+// }
+// if (buttonCheap.checked === true) {
+//   count++;
+// }
+// if (buttonGood.checked === true) {
+//   count++;
+// }
+
+// if (count < 3) {
+//   return false;
+// } else {
+//   return true;
+// }
+
+// // add eventlisteners
+// buttonFast.addEventListener("click", () => {
+//   if (checkTheBoxes() === true) {
+//     lastBox.checked = false;
+//   }
+//   lastBox = buttonFast;
+// });
+
+// buttonCheap.addEventListener("click", () => {
+//   if (checkTheBoxes() === true) {
+//     lastBox.checked = false;
+//   }
+//   lastBox = buttonCheap;
+// });
+
+// buttonGood.addEventListener("click", () => {
+//   if (checkTheBoxes() === true) {
+//     lastBox.checked = false;
+//   }
+//   lastBox = buttonGood;
+// });
