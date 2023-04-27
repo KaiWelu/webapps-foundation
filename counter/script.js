b32a03253088553fd943748f035b9e25dfcf2715
Kai Weluda <kaiweluda@gmail.com>
2023-04-27 10:19:50 +0200

    feat: adds counting when you press enter/space and changes the background

--- counter/script.js
+++ counter/script.js
@@ -3,26 +3,36 @@ let counter = 0;
 const counterDisplay = document.querySelector("main");
 counterDisplay.textContent = counter;
 
-// function to increment the counter
-// const counterIncrement = function () {
-//   if (counter < 100) {
-//     counter++;
-//     counterDisplay.textContent = counter;
-//   } else {
-//     counter = 0;
-//     counterDisplay.textContent = counter;
-//   }
-// };
-
-// adds eventlisteners for clicks and button presses
-counterDisplay.addEventListener("click", function () {
+function incrementCounter() {
   if (counter < 99) {
     counter++;
     counterDisplay.textContent = counter;
+    document.body.style.setProperty("--background-border", counter + "%");
   } else {
     counter = 0;
     counterDisplay.textContent = counter;
+    document.body.style.setProperty("--background-border", counter + "%");
+  }
+}
+
+// adds eventlisteners for clicks
+counterDisplay.addEventListener("click", () => {
+  incrementCounter();
+});
+
+// adds event listener for the enter and space key
+document.addEventListener("keydown", (event) => {
+  if (event.code == "Space") {
+    incrementCounter();
+  } else if (event.code == "Enter") {
+    incrementCounter();
   }
 });
 
 // reset button
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", function () {
+  counter = 0;
+  counterDisplay.textContent = counter;
+  document.body.style.setProperty("--background-border", counter + "%");
+});
