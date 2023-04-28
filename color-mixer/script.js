e5598a1750518c2053b6cf2afd9384239b2a2a37
Kai Weluda <kaiweluda@gmail.com>
2023-04-28 08:54:32 +0200

    chore: refactors changing and setting the background color

--- color-mixer/script.js
+++ color-mixer/script.js
@@ -12,29 +12,22 @@ function toHex(inputString) {
   return output.slice(-2);
 }
 
+// this function sets the background color based on the sliders state
+function changeBackground() {
+  document.body.style.setProperty("--red", redButton.value);
+  document.body.style.setProperty("--green", greenButton.value);
+  document.body.style.setProperty("--blue", blueButton.value);
+}
+
+// sets the initial background
+changeBackground();
+
 // displays the initial color value in hex
 valueDisplay.textContent =
   "#" +
   toHex(redButton.value) +
   toHex(greenButton.value) +
   toHex(blueButton.value);
-
-// this sets the initial background color based on the sliders state
-document.body.style.setProperty("--red", redButton.value);
-document.body.style.setProperty("--green", greenButton.value);
-document.body.style.setProperty("--blue", blueButton.value);
-
-redButton.addEventListener("input", () => {
-  document.body.style.setProperty("--red", redButton.value);
-});
-
-greenButton.addEventListener("input", () => {
-  document.body.style.setProperty("--green", greenButton.value);
-});
-
-blueButton.addEventListener("input", () => {
-  document.body.style.setProperty("--blue", blueButton.value);
-});
 
 // event listener for the hex display
 colorSliders.addEventListener("input", () => {
@@ -44,3 +37,27 @@ colorSliders.addEventListener("input", () => {
     toHex(greenButton.value) +
     toHex(blueButton.value);
 });
+
+// event listener for changes in the sliders
+colorSliders.addEventListener("input", () => {
+  changeBackground();
+});
+
+/* LEGACY CODE AFTER THIS */
+
+// this sets the initial background color based on the sliders state
+// document.body.style.setProperty("--red", redButton.value);
+// document.body.style.setProperty("--green", greenButton.value);
+// document.body.style.setProperty("--blue", blueButton.value);
+
+// redButton.addEventListener("input", () => {
+//   document.body.style.setProperty("--red", redButton.value);
+// });
+
+// greenButton.addEventListener("input", () => {
+//   document.body.style.setProperty("--green", greenButton.value);
+// });
+
+// blueButton.addEventListener("input", () => {
+//   document.body.style.setProperty("--blue", blueButton.value);
+// });
